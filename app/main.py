3ecb97b369ee1db55abd1a5b0d7b183f863e2fad
Gustavo de los Santos <gusleo94@gmail.com>
2023-06-23 01:25:01 +0200

    initial commit

---
+++ app/main.py
@@ -0,0 +1,13 @@
+import customtkinter as ctk
+
+
+class Pomodoro(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1200x800")
+        self.title("PomoWork")
+
+
+if __name__ == "__main__":
+    app = Pomodoro()
+    app.mainloop()
