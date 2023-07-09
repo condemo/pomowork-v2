538fefec0844908c74301af6892668eff1a80c6c
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-09 19:48:36 +0200

    refactor: auth utils refactor

--- app/utils/auth/signup.py
+++ app/utils/auth/signup.py
@@ -1,7 +1,18 @@
+import requests
+import json
 from re import fullmatch
-from data.auth import send_signup_request
+
+from config import SIGNUP_URL
 
 regex = r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,7}\b'
+
+
+def send_signup_request(data: dict) -> dict | Exception:
+    # TODO: Completar la gestion de errores
+    data_send = json.dumps(data)
+    send = requests.post(url=SIGNUP_URL, data=data_send).json()
+
+    return send
 
 
 def signup_handler(username: str, password: str, repeated_password: str, email: str):
