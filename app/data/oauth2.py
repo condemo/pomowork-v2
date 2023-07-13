30999e2fd02f5e9dc38ba18b889164339207386a
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-13 02:20:40 +0200

    style: added commentes

--- app/data/oauth2.py
+++ app/data/oauth2.py
@@ -10,11 +10,12 @@ def save_jwt(data: dict) -> None:
     with open(DATA_DIR + "/.token", "wb") as file:
         file.write(encoded)
 
+    # TODO: Mirar de optimizar: creamos o sobreescribimos .token para luego volverlo a abrir
+    # en encrypt_token_file, posiblemente mejor pasarlo como parametro a esta funcion
     encrypt_token_file()
-    get_token()
 
 
 def get_token():
     token = decrypt_token_file()
 
-    print(base64.b64decode(token).decode("utf-8"))
+    return base64.b64decode(token).decode("utf-8")
