5aefe1cbde1ee0ef2bc2d72fab9ad3c5b1e1b0d8
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-13 02:00:26 +0200

    updated config

--- app/config.py
+++ app/config.py
@@ -1,5 +1,12 @@
+import os
+
+
+CURRENT_DIRECTORY = os.getcwd() + "/app"
+DATA_DIR = CURRENT_DIRECTORY + "/data"
+
+# SERVER DATA
 SERVICE_URL = "http://127.0.0.1:8000"
 PROJECTS_BASE_URL = SERVICE_URL + "/products"
 CARDS_BASE_URL = SERVICE_URL + "/cards"
 LOGIN_URL = SERVICE_URL + "/login"
-SIGNUP_URL = SERVICE_URL + "/users/"
+SIGNUP_URL = SERVICE_URL + "/users"
