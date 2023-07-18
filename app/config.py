a0b0169d54df4f8382291847b4b67eb6c989364a
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-18 21:16:50 +0200

    cleaner urls

--- app/config.py
+++ app/config.py
@@ -5,11 +5,11 @@ CURRENT_DIRECTORY = os.getcwd() + "/app"
 DATA_DIR = CURRENT_DIRECTORY + "/data"
 
 # SERVER DATA
-SERVICE_URL = "http://127.0.0.1:8000"
-PROJECTS_BASE_URL = SERVICE_URL + "/products"
-CARDS_BASE_URL = SERVICE_URL + "/cards"
-LOGIN_URL = SERVICE_URL + "/login"
-SIGNUP_URL = SERVICE_URL + "/users"
+SERVICE_URL = "http://127.0.0.1:8000/"
+PROJECTS_BASE_URL = SERVICE_URL + "projects/"
+CARDS_BASE_URL = SERVICE_URL + "cards/"
+LOGIN_URL = SERVICE_URL + "login"
+SIGNUP_URL = SERVICE_URL + "users/"
 USER_HEADERS = {
     "accept": "application/json",
     "Authorization": "",
