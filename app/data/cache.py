9cba263d99347e6552d7d7a86bcd2714fe7909e8
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-19 18:38:47 +0200

    feat: implemented load projects data

--- app/data/cache.py
+++ app/data/cache.py
@@ -1,8 +1,13 @@
 import requests
-import config
 import os
-from data.oauth2 import get_token
 import json
+
+import config
+from lib.models import Project, Card
+from data.oauth2 import get_token
+
+
+CACHE_FILE = config.DATA_DIR + "/data.json"
 
 
 def cache_fetch():
@@ -12,7 +17,23 @@ def cache_fetch():
     data_fetch = requests.get(
         config.PROJECTS_BASE_URL, headers=user_credentials).json()
 
-    with open(config.DATA_DIR + "/data.json", "w") as file:
+    with open(CACHE_FILE, "w") as file:
         json.dump(data_fetch, file, indent=2)
 
-    os.chmod(config.DATA_DIR + "/data.json", 0o600)
+    os.chmod(CACHE_FILE, 0o600)
+
+
+def load_projects() -> list[Project]:
+    projects_list = []
+    with open(CACHE_FILE, "r") as file:
+        data: dict = json.load(file)
+
+    for i in data:
+        project = Project(**i)
+        project.cards = []
+        for j in i["cards"]:
+            card = Card(**j)
+            project.cards.append(card)
+        projects_list.append(project)
+
+    return projects_list
--- app/views/main/home.py
+++ app/views/main/home.py
@@ -1,4 +1,5 @@
 import customtkinter as ctk
+from data.cache import load_projects
 
 
 class HomeView(ctk.CTkFrame):
@@ -6,8 +7,12 @@ class HomeView(ctk.CTkFrame):
         super().__init__(master=master)
         self.pack_propagate(False)
 
+        self.load_data()
         self.create_widgets()
         self.load_widgets()
+
+    def load_data(self) -> None:
+        self.projects_list = load_projects()
 
     def create_widgets(self) -> None:
         self.title = ctk.CTkLabel(self, text="HOME VIEW", font=("Roboto", 24))
