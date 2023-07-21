f3b35f235987fb407d1a513c37576c8a8e360b1f
Gustavo de los Santos <gusleo94@gmail.com>
2023-07-22 01:06:35 +0200

    feat: completed projects frame layout

--- app/views/main/frames/projects.py
+++ app/views/main/frames/projects.py
@@ -2,10 +2,39 @@ import customtkinter as ctk
 from data.cache import load_projects
 
 
-class ProjectsFrame(ctk.CTkScrollableFrame):
+class ProjectsFrame(ctk.CTkFrame):
     def __init__(self, master):
-        super().__init__(
-            master=master, width=30)
+        super().__init__(master=master, width=20)
+
+        self.create_widgets()
+        self.load_widgets()
+
+    def create_widgets(self) -> None:
+        self.top_frame = ctk.CTkFrame(self, fg_color="transparent")
+        self.burger_btn = ctk.CTkButton(
+            self.top_frame, text="III", width=30, height=30, corner_radius=60)
+
+        self.mid_frame = ProjectsCardFrame(self)
+
+        self.bottom_frame = ctk.CTkFrame(self)
+        self.add_btn = ctk.CTkButton(self.bottom_frame, text="ADD")
+
+    def load_widgets(self) -> None:
+        self.burger_btn.pack(side="right")
+        self.top_frame.pack(fill="x", pady=10, padx=5)
+
+        self.mid_frame.show()
+
+        self.add_btn.pack(fill="x")
+        self.bottom_frame.pack(fill="x", pady=10, padx=5)
+
+    def show(self) -> None:
+        self.pack(side="left", expand=True, fill="both")
+
+
+class ProjectsCardFrame(ctk.CTkScrollableFrame):
+    def __init__(self, master):
+        super().__init__(master=master, width=20)
 
         self.load_data()
         self.create_widgets()
@@ -15,16 +44,16 @@ class ProjectsFrame(ctk.CTkScrollableFrame):
         self.projects_list = load_projects()
 
     def create_widgets(self) -> None:
-        self.profile_list = [ProjectProfile(self, i.id, i.name) for i in self.projects_list]
+        self.profile_list = [ProjectProfileCard(self, i.id, i.name) for i in self.projects_list]
 
     def load_widgets(self) -> None:
         [i.show() for i in self.profile_list]
 
     def show(self) -> None:
-        self.pack(side="left", expand=True, fill="both")
+        self.pack(expand=True, fill="both")
 
 
-class ProjectProfile(ctk.CTkFrame):
+class ProjectProfileCard(ctk.CTkFrame):
     def __init__(self, master, id: int, name: str):
         super().__init__(
             master=master, fg_color="orange", corner_radius=5,
@@ -43,10 +72,12 @@ class ProjectProfile(ctk.CTkFrame):
     def create_widgets(self) -> None:
         self.name_label = ctk.CTkLabel(
             self, text=self.name, font=("Roboto", 18), bg_color="transparent")
+        self.config_btn = ctk.CTkButton(self, text="C", fg_color="brown")
 
     def load_widgets(self) -> None:
         self.name_label.grid(
             column=0, row=0, rowspan=2, sticky="nswe", padx=5, pady=5)
+        self.config_btn.grid(column=1, row=0, rowspan=2, padx=5)
 
     def show(self) -> None:
         self.pack(fill="x", pady=5)
