5a1c724630b4e4a4cdc25cb0ad974d896896c2c6
Gustavo de los Santos <gusleo94@gmail.com>
2023-08-21 20:43:13 +0200

    refactor: better card order

--- app/data/datahandlers.py
+++ app/data/datahandlers.py
@@ -14,4 +14,13 @@ class ProjectDataHandler:
 
     def get_project_cards(self) -> list[Card]:
         card_list = self.cache_handler.get_card_list(self.current_project_id)
-        return [Card(**card) for card in card_list]
+        card_list.sort(key=lambda e: e["id"])
+        card_list.reverse()
+        self.card_list = [Card(**card) for card in card_list]
+        return self.card_list
+
+    def create_card(self) -> Card:
+        pass
+
+    def change_current_project(self, id: int) -> None:
+        self.current_project_id = id
