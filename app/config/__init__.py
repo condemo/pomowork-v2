bf423d989af69170efca6d85c46e5b3b64056c93
Gustavo de los Santos <gusleo94@gmail.com>
2023-09-18 02:25:16 +0200

    feat: refresh_url added to config

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -11,8 +11,9 @@ USER_CONF_FILE = CONFIG_FOLDER + "/user_conf.toml"
 SERVICE_URL = "http://127.0.0.1:8000/"
 PROJECTS_BASE_URL = SERVICE_URL + "projects/"
 CARDS_BASE_URL = SERVICE_URL + "cards/"
-LOGIN_URL = SERVICE_URL + "login"
+LOGIN_URL = SERVICE_URL + "login/"
 SIGNUP_URL = SERVICE_URL + "users/"
+REFRESH_URL = LOGIN_URL + "refresh/"
 USER_HEADERS = {
     "accept": "application/json",
     "Authorization": "",
