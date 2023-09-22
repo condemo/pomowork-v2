16a42c71bd5cc0c2dc020134131de56df8f17e57
Gustavo de los Santos <gusleo94@gmail.com>
2023-09-22 17:09:17 +0200

    refactor: main.py -> pomowork.py

---
+++ app/pomowork.py
@@ -0,0 +1,21 @@
+import customtkinter as ctk
+
+from views.utils import ViewController
+from utils.auth import JWTChecker
+
+
+class PomoWork(ctk.CTk):
+    def __init__(self):
+        super().__init__()
+        self.geometry("1400x800")
+        self.title("PomoWork")
+
+        self.credentials_view = JWTChecker()
+        self.view_controller = ViewController()
+
+        self.view_controller.change_view(self.credentials_view.check_credentials(), self)
+
+
+if __name__ == "__main__":
+    app = PomoWork()
+    app.mainloop()
