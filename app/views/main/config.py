9d2761b1f9378e7a00f4bdbc4ce2aa8ff3b4e6c1
Gustavo de los Santos <gusleo94@gmail.com>
2023-10-09 20:53:21 +0200

    feat:added widgets schema

--- app/views/main/config.py
+++ app/views/main/config.py
@@ -10,3 +10,18 @@ class ConfigWindow(ctk.CTkToplevel):
         self.title("Configuración")
 
         self.data_handler = data_handler
+
+        self.create_widgets()
+        self.load_widgets()
+
+    def create_widgets(self) -> None:
+        self.menu_frame = ctk.CTkFrame(self, fg_color="blue")
+        self.logout_btn = ctk.CTkButton(
+            self.menu_frame, text="Logout", fg_color="red",
+            font=("Roboto", 18, "bold"), corner_radius=10)
+        self.section_frame = ctk.CTkFrame(self, fg_color="yellow")
+
+    def load_widgets(self) -> None:
+        self.menu_frame.pack(side="left", fill="y")
+        self.logout_btn.place(anchor="sw", relx=0, rely=1, relwidth=1, relheight=.06)
+        self.section_frame.pack(expand=True, fill="both", side="left")
