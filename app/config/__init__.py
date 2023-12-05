c3402729e11f47fad23abf3d1793653574ec0524
Gustavo de los Santos <gustleo.dev@gmail.com>
2023-12-05 12:10:04 +0100

    version 0.5.1 -> 0.5.2

--- app/config/__init__.py
+++ app/config/__init__.py
@@ -2,7 +2,7 @@ import os
 import pathlib
 import tomlkit
 
-_VERSION = "0.5.1-alpha"
+_VERSION = "0.5.2-alpha"
 
 SYS_CONFIG_DIR = os.path.expanduser("~/.config/")
 CONFIG_FOLDER = os.path.join(SYS_CONFIG_DIR, "pomowork")
