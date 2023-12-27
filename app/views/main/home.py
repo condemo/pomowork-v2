d2287ed042f197f356cd2141d952ee36df57bc9a
Gustavo de los Santos <gustleo.dev@gmail.com>
2023-12-27 20:12:16 +0100

    refactor: removed `core_view`

--- app/views/main/home.py
+++ app/views/main/home.py
@@ -22,16 +22,13 @@ class HomeView(ctk.CTkFrame):
 
     def create_widgets(self) -> None:
         self.projects_frame = ProjectsFrame(self, self.data_handler)
-        self.core_view = ctk.CTkFrame(self, fg_color=Colors.TRANSPARENT)
-        self.main_frame = MainFrame(self.core_view, self.data_handler)
-        self.cards_frame = CardsFrame(self.core_view, self.data_handler)
+        self.main_frame = MainFrame(self, self.data_handler)
+        self.cards_frame = CardsFrame(self, self.data_handler)
 
     def load_widgets(self) -> None:
         self.projects_frame.show()
-        if self.data_handler.get_project_list():
-            self.core_view.pack(expand=True, fill="both")
-            self.main_frame.show()
-            self.cards_frame.show()
+        self.main_frame.show()
+        self.cards_frame.show()
 
     def change_active_project(self, id: int) -> None:
         self.data_handler.change_current_project(id)
